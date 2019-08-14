da34374519fe36ca32da9fa3ee9bcb886909b844
Siddiqui <31212069+dirtydeM0n@users.noreply.github.com>
2019-08-14 19:19:17 +0500

    General routes implemented

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const config = require('config'); // Npm module config
+const mongoose = require('mongoose'); // Npm module mongoose
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    //Exiting the process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB(); // Calling MongoDB
 
 const PORT = process.env.PORT || 5000;
 
+// Get request
 app.get('/', (req, res) => res.send('Express server online. API running'));
+
+// Defining routes
+// One for each file
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 // template literal on PORT variable
